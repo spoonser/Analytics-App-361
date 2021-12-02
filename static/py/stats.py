259a5919f2ef4414eeb0fa290690785939cb1a49
spencer <spencerwagner0@gmail.com>
2021-12-02 10:42:39 -0800

    Removed unused directories (javascript, images)

---
+++ static/py/stats.py
@@ -0,0 +1,18 @@
+# ***************************************************************************
+# * CS361 Project - Analytics Application 
+# * Spencer Wagner
+# * Stats functions for analytics application
+# ***************************************************************************
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from pandas.core.base import DataError
+import numpy as np
+import pandas as pd
+import io
+import base64
+
+
+# ***************************************************************************
+# * BASIC STATS - Mean, median, mode, standard deviation 
+# ***************************************************************************
